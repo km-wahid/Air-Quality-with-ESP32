d4e053603fdde5602f8361ff5c5a20f41a28f44c
khalid <109499271+Khalidmuhammad25@users.noreply.github.com>
2025-10-19 00:03:12 +0600

    ip and graphana update

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5#sjnh1@9jmls18cep55m@bh!$mv#qdz8ce-hp0qt9bhj(laih
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.201.68.179", "27.0.0.1:8000"]
 
 
 # Application definition
--- grafana/data/plugins/grafana-metricsdrilldown-app/CHANGELOG.md
+++ grafana/data/plugins/grafana-metricsdrilldown-app/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v1.0.18
+
+See <https://github.com/grafana/metrics-drilldown/releases/tag/v1.0.18>
+
 ## v1.0.17
 
 See <https://github.com/grafana/metrics-drilldown/releases/tag/v1.0.17>
